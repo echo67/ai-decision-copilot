45c4dcf3081d525fe3cbae767fe38342b76e7b43
Eunah Cho <eacho067@gmail.com>
2026-07-09 01:11:30 +0900

    Add interactive visualization dashboard

--- app/main.py
+++ app/main.py
@@ -41,3 +41,71 @@ st.dataframe(df.describe())
 
 st.subheader("Preview")
 st.dataframe(df.head())
+
+
+# import matplotlib.pyplot as plt
+# numeric_cols = df.select_dtypes(include="number").columns
+# selected_col = st.selectbox(
+#     "수치형 컬럼 선택",
+#     numeric_cols
+# )
+# fig, ax = plt.subplots()
+# ax.hist(df[selected_col].dropna())
+# ax.set_title(selected_col)
+# ax.set_xlabel(selected_col)
+# ax.set_ylabel("Count")
+# st.pyplot(fig)
+
+import plotly.express as px
+
+plot_type = st.selectbox(
+    "그래프 종류",
+    [
+        "Histogram",
+        "Box Plot",
+        "Violin Plot"
+    ]
+)
+
+numeric_cols = df.select_dtypes(include="number").columns
+
+selected_col = st.selectbox(
+    "수치형 컬럼 선택",
+    numeric_cols
+)
+
+if plot_type == "Histogram":
+    fig = px.histogram(
+        df,
+        x=selected_col,
+        nbins=30,
+        title=f"{selected_col} Distribution"
+    )
+elif plot_type == "Box Plot":
+    fig = px.box(
+        df,
+        y=selected_col,
+        title=f"{selected_col} Box Plot"
+    )
+elif plot_type == "Violin Plot":
+    fig = px.violin(
+        df,
+        y=selected_col,
+        box=True,
+        title=f"{selected_col} Violin Plot"
+    )
+
+fig.update_layout(
+    xaxis_title=selected_col,
+    yaxis_title="Count",
+    template="plotly_white",
+    height = 500
+)
+
+st.plotly_chart(
+    fig,
+    use_container_width=True
+)
+
+
+
